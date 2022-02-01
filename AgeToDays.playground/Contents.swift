53a0fd7ba41926eebb67ea8f5015b18c80c715e3
Decagon <mac@macs-MacBook-Pro.local>
2022-02-01 14:41:43 +0100

    function to convert the number of age to total number of days

---
+++ AgeToDays.playground/Contents.swift
@@ -0,0 +1,18 @@
+import UIKit
+
+//creating a function that returns the number of years as total days in that year
+//Taking the total days in a year as 365 days, ignoring leaps years or any other variable
+
+func ageToDays(_age: Int) ->Int {
+    
+    let age = _age
+    
+    let days = age * 365
+    return days
+}
+
+ageToDays(_age: 5)
+
+ageToDays(_age: 2)
+
+ageToDays(_age: 10)
